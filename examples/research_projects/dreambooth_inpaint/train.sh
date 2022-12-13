624dc260ce44f950a47f0c0253726698efd2d4e6
Hao Chen <stanzju@gmail.com>
2022-12-13 15:46:24 +0800

    add dreambooth inpaint

---
+++ examples/research_projects/dreambooth_inpaint/train.sh
@@ -0,0 +1,16 @@
+export MODEL_NAME="CompVis/stable-diffusion-v1-4"
+export INSTANCE_DIR="imgs/manny"
+export OUTPUT_DIR="outputs/manny"
+
+accelerate launch train_dreambooth_inpaint.py \
+  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --instance_data_dir=$INSTANCE_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --instance_prompt="a photo of sks dog" \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=1 \
+  --learning_rate=5e-6 \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --max_train_steps=400
